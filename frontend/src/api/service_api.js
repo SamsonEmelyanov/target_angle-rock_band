c8163a20bfe8d08dabb0606a50ea818961b11d33
Samson Emelyanov <samson.emelyanov@gmail.com>
2021-12-13 01:29:06 +0300

    13.12.2021/01:28/update

--- frontend/src/api/service_api.js
+++ frontend/src/api/service_api.js
@@ -1,14 +1,7 @@
 import axios from 'axios';
 
-
-export const authServiceAPI = axios.create({
-    baseURL: 'http://localhost:8080'
-})
-
 export const commonServiceAPI = axios.create({
     baseURL: 'http://localhost:8080'
 })
 
-export const searchSuggestionServiceAPI = axios.create({
-    baseURL: 'http://localhost:8080'
-})
+
