94c0b502aa290ca6a71d5a0d94f2c9711d297254
Samson Emelyanov <samson.emelyanov@gmail.com>
2023-09-17 19:07:28 +0400

    17.09.2023/19:07/update

--- README.md
+++ README.md
@@ -1,10 +1,9 @@
 # target_angle-rock_band
 1. install npm package(node_modules) to local folder according to package.json dependencies
 2. create schema 'target_angle' at the workbench & fill application.yaml in 'backend/src/main/recources' according to your personal data(email service login & password, MySQL DB password)
-3. run initial_data.sql script in 'backend/src/main/recources' folder
-4. start backend part(TargetAngleApplication.java) in 'backend/src/main/java/com/example/targetangle/' folder on 8080 port
-5. start frontend part in 'frontend/src' folder using 'npm start' on 3000 port
-6. For testing payments use: NUMBER: 4242424242424242,
+3. start backend part(TargetAngleApplication.java) in 'backend/src/main/java/com/example/targetangle/' folder on 8080 port
+4. start frontend part in 'frontend/src' folder using 'npm start' on 3000 port
+5. For testing payments use: NUMBER: 4242424242424242,
                             BRAND: Visa,
                             CVC: Any 3 digits,
                             DATE: Any future date.
--- backend/src/main/resources/schema.sql
+++ backend/src/main/resources/schema.sql
@@ -1,3 +1,18 @@
+DROP TABLE IF EXISTS `address_info`;
+DROP TABLE IF EXISTS `apparel_category`;
+DROP TABLE IF EXISTS `carousel_images`;
+DROP TABLE IF EXISTS `chat_message`;
+DROP TABLE IF EXISTS `users`;
+DROP TABLE IF EXISTS `contact_info`;
+DROP TABLE IF EXISTS `gender_category`;
+DROP TABLE IF EXISTS `price_range_category`;
+DROP TABLE IF EXISTS `product_brand_category`;
+DROP TABLE IF EXISTS `apparel_images`;
+DROP TABLE IF EXISTS `bank_info`;
+DROP TABLE IF EXISTS `brand_images`;
+DROP TABLE IF EXISTS `order_info`;
+DROP TABLE IF EXISTS `product_info`;
+
 CREATE TABLE `address_info` (
                                 `id` int NOT NULL AUTO_INCREMENT,
                                 `country` varchar(255) DEFAULT NULL,
@@ -157,11 +172,6 @@ CREATE TABLE `product_info` (
                                 CONSTRAINT `FKaoybeugv1xmsh079m2i89p7ug` FOREIGN KEY (`apparel_id`) REFERENCES `apparel_category` (`id`),
                                 CONSTRAINT `FKbuskw9m5b0ji86wihiw38dfmt` FOREIGN KEY (`gender_id`) REFERENCES `gender_category` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-INSERT INTO target_angle.product_info (id, available_quantity, delivery_time, image_local_path, imageurl, name, price, publication_date, ratings, seller_id, verification_status, apparel_id, gender_id, price_id, brand_id) VALUES (1, 0, 0, null, 'https://firebasestorage.googleapis.com/v0/b/target-angle-rock-band.appspot.com/o/target-angle-stuff%2F%D0%A4%D1%83%D1%82%D0%B1%D0%BE%D0%BB%D0%BA%D0%B0.png?alt=media&token=63e6a2d7-df96-44c5-82b8-f50268e9851c', 'Футболка', 1500, null, 0, 1, false, null, null, null, null);
-INSERT INTO target_angle.product_info (id, available_quantity, delivery_time, image_local_path, imageurl, name, price, publication_date, ratings, seller_id, verification_status, apparel_id, gender_id, price_id, brand_id) VALUES (2, 0, 0, null, 'https://firebasestorage.googleapis.com/v0/b/target-angle-rock-band.appspot.com/o/target-angle-stuff%2F%D0%9A%D1%80%D1%83%D0%B6%D0%BA%D0%B0.png?alt=media&token=7a9036d6-0ee5-4b31-ba47-04e4b48a974f', 'Кружка', 500, null, 0, 1, false, null, null, null, null);
-INSERT INTO target_angle.product_info (id, available_quantity, delivery_time, image_local_path, imageurl, name, price, publication_date, ratings, seller_id, verification_status, apparel_id, gender_id, price_id, brand_id) VALUES (3, 0, 0, null, 'https://firebasestorage.googleapis.com/v0/b/target-angle-rock-band.appspot.com/o/target-angle-stuff%2F%D0%91%D1%80%D0%B0%D1%81%D0%BB%D0%B5%D1%82.png?alt=media&token=d0727906-f767-4177-948b-95ea2eabf25b', 'Браслет', 1000, null, 0, 1, false, null, null, null, null);
-INSERT INTO target_angle.product_info (id, available_quantity, delivery_time, image_local_path, imageurl, name, price, publication_date, ratings, seller_id, verification_status, apparel_id, gender_id, price_id, brand_id) VALUES (4, 0, 0, null, 'https://firebasestorage.googleapis.com/v0/b/target-angle-rock-band.appspot.com/o/target-angle-stuff%2F%D0%91%D0%B8%D1%80%D0%BA%D0%B0.png?alt=media&token=05593dea-3688-49fa-b7e6-39a9792518f0', 'Бирка', 500, null, 0, 1, false, null, null, null, null);
 
 DROP TABLE IF EXISTS QRTZ_FIRED_TRIGGERS;
 DROP TABLE IF EXISTS QRTZ_PAUSED_TRIGGER_GRPS;
@@ -332,5 +342,3 @@ CREATE INDEX IDX_QRTZ_FT_J_G ON QRTZ_FIRED_TRIGGERS(SCHED_NAME,JOB_NAME,JOB_GROU
 CREATE INDEX IDX_QRTZ_FT_JG ON QRTZ_FIRED_TRIGGERS(SCHED_NAME,JOB_GROUP);
 CREATE INDEX IDX_QRTZ_FT_T_G ON QRTZ_FIRED_TRIGGERS(SCHED_NAME,TRIGGER_NAME,TRIGGER_GROUP);
 CREATE INDEX IDX_QRTZ_FT_TG ON QRTZ_FIRED_TRIGGERS(SCHED_NAME,TRIGGER_GROUP);
-
-commit;
